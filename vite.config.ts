e562121f353dfe57eed799e41ad15e169523787b
yinchengnuo <yinchengnuo@qq.com>
2021-11-02 23:28:58 +0800

    发布 v1.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+/*
+ * @Description: vite.config.ts
+ * @Autor: 尹成诺
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
+  base: '/yapi',
   resolve: {
     alias: {
       '@': '/src'
