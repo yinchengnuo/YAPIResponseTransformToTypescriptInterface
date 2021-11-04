2afb37c4d59b60bda4285612260a50f7bf2e334b
yinchengnuo <yinchengnuo@qq.com>
2021-11-04 15:00:32 +0800

    修改 base 为 /

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
-  base: '/app/yapi/',
   resolve: {
     alias: {
       '@': '/src'
