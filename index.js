39fd066da30b0c4487aef3faa1039c862bceb85b
尹成诺 <yinchengnuo@qq.com>
2021-11-23 11:39:25 +0800

    优化 Electron 配置

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const { app, BrowserWindow } = require('electron')
-
-const dev = (process.env.npm_lifecycle_script || '').includes('--dev')
+const { app, BrowserWindow, Menu } = require('electron')
 
 app.whenReady().then(() => {
-  const mainWindow = new BrowserWindow()
-  dev ? mainWindow.loadURL('http://localhost:3000') : mainWindow.loadFile('dist/index.html')
+  Menu.setApplicationMenu(Menu.buildFromTemplate([]))
+
+  const mainWindow = new BrowserWindow({
+    show: false, // 默认不显示
+    resizable: false, // 不允许调整窗口尺寸
+    // fullscreen: true, // 默认全屏
+    // alwaysOnTop: true, // 窗口置顶
+    useContentSize: true // 使用内容区域宽高作为窗口宽高
+  })
+
+  // 页面加载完成后显示
+  mainWindow.once('ready-to-show', mainWindow.show)
+
+  app.isPackaged ? mainWindow.loadFile('build/index.html') : mainWindow.loadURL('http://localhost:3000')
 })
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+app.on('window-all-closed', app.quit)
--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
     }
   },
   build: {
+    outDir: 'build',
     chunkSizeWarningLimit: 2048
   }
 })
